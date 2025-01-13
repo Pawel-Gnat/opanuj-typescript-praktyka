8d23f821e30340a27863a0ba1bc5d39d467022d0
Pawel Gnat <pawel_gnat@o2.pl>
2025-01-13 20:43:51 +0100

    chore: resolve 421

--- tasks/core-pro/421-literals-code/task.ts
+++ tasks/core-pro/421-literals-code/task.ts
@@ -1,5 +1,13 @@
-export type Code = null;
+type SingleNumber = 0 | 1;
+
+export type Code =
+  `${SingleNumber}${SingleNumber}${SingleNumber}-${SingleNumber}${SingleNumber}${SingleNumber}-${SingleNumber}${SingleNumber}${SingleNumber}`;
 
 export function codeToDecimal(code: Code) {
-  throw new Error('Not implemented');
+  const groups = code.split('-');
+  const decimalGroups = groups.map((group) => {
+    return parseInt(group, 2);
+  });
+
+  return decimalGroups.join('');
 }

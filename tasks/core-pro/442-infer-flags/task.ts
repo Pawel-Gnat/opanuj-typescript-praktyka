5aef2a75e2e401fa627f7e37476efd85a4def508
Pawel Gnat <pawel_gnat@o2.pl>
2025-01-14 20:47:39 +0100

    chore: resolve 442

--- tasks/core-pro/442-infer-flags/task.ts
+++ tasks/core-pro/442-infer-flags/task.ts
@@ -1,11 +1,17 @@
 import { type FeatureFlags } from './legacy-flags.ts';
 
-type FlagsV2<T> = {};
+type FlagsV2<T> = {
+  [K in keyof T as K extends `${infer Base}V2Enabled` ? `${Base}Enabled` : never]: T[K];
+};
 
 export type ModernFeatureFlags = FlagsV2<FeatureFlags>;
 
 export function getFeatureFlagsV2(flags: FeatureFlags): ModernFeatureFlags {
-  const flagsV2: ModernFeatureFlags = {};
+  const flagsV2: ModernFeatureFlags = Object.fromEntries(
+    Object.entries(flags)
+      .filter(([key]) => key.endsWith('V2Enabled'))
+      .map(([key, value]) => [key.replace('V2Enabled', 'Enabled'), value]),
+  ) as ModernFeatureFlags;
 
   return flagsV2;
 }

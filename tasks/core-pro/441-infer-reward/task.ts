e79b9bac991a42d7a84d750337ca7c92b1d28a6d
Pawel Gnat <pawel_gnat@o2.pl>
2025-01-14 20:36:18 +0100

    chore: resolve 441

--- tasks/core-pro/441-infer-reward/task.ts
+++ tasks/core-pro/441-infer-reward/task.ts
@@ -1,1 +1,7 @@
-export type RewardRadar = '🔍';
+export type RewardRadar<T extends string> = T extends `${string}[${infer Reward}]${infer Rest}`
+  ? Reward extends `${infer Num}$`
+    ? Num extends '0'
+      ? RewardRadar<Rest>
+      : `${Num}$`
+    : RewardRadar<Rest>
+  : null;

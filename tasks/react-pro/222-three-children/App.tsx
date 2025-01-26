a278f440bac83bb22bdac5e37af3131c8f8e8d9b
Pawel Gnat <pawel_gnat@o2.pl>
2025-01-26 12:47:09 +0100

    chore: resolve 222

--- tasks/react-pro/222-three-children/App.tsx
+++ tasks/react-pro/222-three-children/App.tsx
@@ -2,7 +2,13 @@
   Dodaj typowanie propsa children dla TripleContainer, które wymusi przekazanie dokładnie trzech komponentów lub elementów HTML.
 */
 
-const TripleContainer = ({ children }) => <ul className="list-disc">{children}</ul>;
+type TripleContainerProps = {
+  children: [React.ReactElement, React.ReactElement, React.ReactElement];
+};
+
+const TripleContainer = ({ children }: TripleContainerProps) => (
+  <ul className="list-disc">{children}</ul>
+);
 
 const App = () => (
   <TripleContainer>

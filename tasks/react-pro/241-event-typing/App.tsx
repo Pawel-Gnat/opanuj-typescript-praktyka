46ebdedfe93a33b48533f17eb5d754c30bf2225a
Pawel Gnat <pawel_gnat@o2.pl>
2025-01-27 21:08:49 +0100

    chore: resolve 241

--- tasks/react-pro/241-event-typing/App.tsx
+++ tasks/react-pro/241-event-typing/App.tsx
@@ -1,7 +1,9 @@
 /* Dodaj odpowiednie typy do event handlera, który pozwoli na dynamiczne wnioskowanie typu eventu "onChange" na elemencie <select> */
 
+import { ComponentProps } from 'react';
+
 const App = () => {
-  const handleChange = (e) => {
+  const handleChange: ComponentProps<'select'>['onChange'] = (e) => {
     console.log('Select option changed:', e.currentTarget.value);
   };
 
